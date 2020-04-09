6e0b2dc2025af8e1c293e95ee5fadc14aaa6a77f
paul <taeyang.kim@sk.com>
2020-04-09 14:52:13 +0900

    ..

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -17,14 +17,12 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model) {
-    //public String index(Model model, @LoginUser SessionUser user) {
-
+    public String index(Model model, @LoginUser SessionUser user) {
             model.addAttribute("posts",postsService.findAllDesc());
-/*
+
         if( user != null )
             model.addAttribute("userName", user.getName());
-*/
+
         return "index";
     }
 
