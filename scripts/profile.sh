ede68569b2bf4a29f12aca08f60eb3fd51934302
paul <taeyang.kim@sk.com>
2020-04-21 13:47:10 +0900

    ..

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -10,8 +10,7 @@ function find_idle_profile()
     then
         CURRENT_PROFILE=real2
     else
-        CURRENT_PROFILE=real1
-        #CURRENT_PROFILE=$(curl -s http://localhost/profile)
+        CURRENT_PROFILE=$(curl -s http://localhost/profile)
     fi
 
     if [ ${CURRENT_PROFILE} == real1 ]
